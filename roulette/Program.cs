9f98d7e63b7c6b4e66c83ffd780d182db79cbff1
SandorEriksson <14SE0303@amal.se>
2025-09-15 11:01:14 +0200

    Add project files.

---
+++ roulette/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("pick a number between 0 and 36");
+    Console.ReadLine();
+
+
+
+Random rand = new Random();
+int roulette = rand.Next(0, 36);
+int guess = Convert.ToInt32(Console.ReadLine());
